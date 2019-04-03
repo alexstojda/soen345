0b530b902003b1a6c4d2336a84c5425858071a3c
NicoChampagne <nicolas.b.champagne@gmail.com>
2019-04-02 21:38:47 -0400

    Added a log message to welcome controller to demo how to log with spring boot

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -1,14 +1,22 @@
 package org.springframework.samples.petclinic.system;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 class WelcomeController {
 
+    private Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
     @GetMapping("/")
     public String welcome() {
+
+        if (logger.isInfoEnabled()) {
+            logger.info("Logging info level enabled and Welcome has been executed");
+        }
+
         return "welcome";
     }
 }
