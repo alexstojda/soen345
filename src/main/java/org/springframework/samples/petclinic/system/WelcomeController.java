cf472db50cffb3a6cb687ce7ce40afbfc54ea641
Mira <miramarhaba@hotmail.com>
2019-04-15 16:46:50 -0400

    add the welcomePageVetToggle implementation

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -3,8 +3,13 @@ package org.springframework.samples.petclinic.system;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.samples.petclinic.LastPage;
+import org.springframework.samples.petclinic.owner.Owner;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Map;
 
 @Controller
 class WelcomeController {
@@ -17,8 +22,16 @@ class WelcomeController {
         if (lastPage.getLastPagePath().equals("/owners/find")) {
             Toggle.logFindOwnerData("Visited Welcome Page\n");
         }
+        Toggle.logWelcomePageVetData("Visited Welcome Page\n");
         lastPage.setLastPagePath("/");
         logger.info("Homepage accessed, Welcome has been returned");
-        return "welcome";
+
+        return Toggle.getWelcomePageVetToggle() ? "welcomePageVetOn" : "welcome";
+    }
+
+    @GetMapping("/toggle")
+    public RedirectView redirectWithUsingRedirectView() {
+        Toggle.toggleWelcomePageVet();
+        return new RedirectView("/");
     }
 }
