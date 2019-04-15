a0deb83c4e72113d97b387d18a5560dfe947c565
Mira <miramarhaba@hotmail.com>
2019-04-15 17:08:40 -0400

    add welcomePageVetToggle tests

--- src/test/java/org/springframework/samples/petclinic/system/ToggleTest.java
+++ src/test/java/org/springframework/samples/petclinic/system/ToggleTest.java
@@ -19,11 +19,23 @@ public class ToggleTest {
     }
 
     @Test
-    public void testToggle(){
+    public void testFindOwnerToggle(){
         toggle.setFindOwnerToggle(false);
         boolean before = toggle.getFindOwnerToggle();
         toggle.toggleFindOwner();
         boolean after = toggle.getFindOwnerToggle();
+
+        assertFalse(before);
+        assertTrue(after);
+    }
+
+
+    @Test
+    public void testWelcomePageVetToggle(){
+        toggle.setWelcomePageVetToggle(false);
+        boolean before = toggle.getWelcomePageVetToggle();
+        toggle.toggleWelcomePageVet();
+        boolean after = toggle.getWelcomePageVetToggle();
 
         assertFalse(before);
         assertTrue(after);
@@ -53,6 +65,29 @@ public class ToggleTest {
     }
 
     @Test
+    public void testToggleLogsForOldWelcomePageVetAreGenerated() throws IOException {
+        toggle.setWelcomePageVetToggle(false);
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/logging/oldWelcomePageVet.txt"));
+
+        int linesBefore = 0;
+        int linesAfter;
+
+        while (reader.readLine() != null){
+            linesBefore++;
+        }
+
+        toggle.logWelcomePageVetData("test\n");
+        linesAfter = linesBefore;
+
+        while (reader.readLine() != null){
+            linesAfter++;
+        }
+        reader.close();
+
+        assertTrue(linesBefore<linesAfter);
+    }
+
+    @Test
     public void testToggleLogsForNewFindOwnerAreGenerated() throws IOException {
         toggle.setFindOwnerToggle(true);
         BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/logging/newFindOwner.txt"));
@@ -65,6 +100,29 @@ public class ToggleTest {
         }
 
         toggle.logFindOwnerData("test\n");
+        linesAfter = linesBefore;
+
+        while (reader.readLine() != null){
+            linesAfter++;
+        }
+        reader.close();
+
+        assertTrue(linesBefore<linesAfter);
+    }
+
+    @Test
+    public void testToggleLogsForNewWelcomePageVetAreGenerated() throws IOException {
+        toggle.setWelcomePageVetToggle(true);
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/logging/newWelcomePageVet.txt"));
+
+        int linesBefore = 0;
+        int linesAfter;
+
+        while (reader.readLine() != null){
+            linesBefore++;
+        }
+
+        toggle.logWelcomePageVetData("test\n");
         linesAfter = linesBefore;
 
         while (reader.readLine() != null){
