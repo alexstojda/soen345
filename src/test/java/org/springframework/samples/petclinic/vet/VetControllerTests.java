6332acfa9f890678f71042f7a2fc701ce79e16eb
EvanDime <evan-dimopoulos@hotmail.com>
2019-04-15 18:24:20 -0400

    updated the test to take into account the toggle that changes the vet page.

--- src/test/java/org/springframework/samples/petclinic/vet/VetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/vet/VetControllerTests.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.samples.petclinic.system.Toggle;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -52,10 +53,17 @@ public class VetControllerTests {
 
     @Test
     public void testShowVetListHtml() throws Exception {
-        mockMvc.perform(get("/vets.html"))
-            .andExpect(status().isOk())
-            .andExpect(model().attributeExists("vets"))
-            .andExpect(view().name("vets/vetList"));
+        if (!Toggle.getPaymentToggle()) {
+            mockMvc.perform(get("/vets.html"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("vets"))
+                .andExpect(view().name("vets/vetList"));
+        } else {
+            mockMvc.perform(get("/vets.html"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("vets"))
+                .andExpect(view().name("vets/vetList2"));
+        }
     }
 
     @Test
