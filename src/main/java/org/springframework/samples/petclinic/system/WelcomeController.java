5d52147b8eee49f75775f431edfe5bf731413648
Mira <miramarhaba@hotmail.com>
2019-04-15 22:17:27 -0400

    adding the hypothesis analysis for homepageVetToggle

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -3,13 +3,13 @@ package org.springframework.samples.petclinic.system;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.samples.petclinic.LastPage;
-import org.springframework.samples.petclinic.owner.Owner;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 @Controller
 class WelcomeController {
@@ -29,9 +29,58 @@ class WelcomeController {
         return Toggle.getWelcomePageVetToggle() ? "welcomePageVetOn" : "welcome";
     }
 
-    @GetMapping("/toggle")
-    public RedirectView redirectWithUsingRedirectView() {
+    @GetMapping("/welcomePageVetToggle")
+    public RedirectView redirectAfterToggle() {
         Toggle.toggleWelcomePageVet();
+        return new RedirectView("/");
+    }
+
+    @GetMapping("/welcomePageVetAnalysis")
+    public RedirectView redirectAfterAnalysis() throws IOException {
+        Toggle.logWelcomePageVetData("Analyzing Data. Null Hypothesis for welcomePageVetToggle: Toggle ON will generate the same number of clicks to Vet button as Toggle OFF.\n");
+
+        double visitedHomepageToggleOn = 0;
+        double visitedHomepageToggleOff = 0;
+        double visitedVetToggleOn = 0;
+        double visitedVetToggleOff = 0;
+
+        BufferedReader logReader;
+        String line;
+
+        logReader = new BufferedReader(new FileReader("src/main/resources/logging/newWelcomePageVet.txt"));
+        line = logReader.readLine();
+        while ((line != null)) {
+            if(line.equals("Visited Welcome Page")){
+                visitedHomepageToggleOn++;
+            } else if(line.equals("Visited Vet Page")){
+                visitedVetToggleOn++;
+            }
+
+            line = logReader.readLine();
+        }
+
+        logReader = new BufferedReader(new FileReader("src/main/resources/logging/oldWelcomePageVet.txt"));
+        line = logReader.readLine();
+        while ((line != null)) {
+            if(line.equals("Visited Welcome Page")){
+                visitedHomepageToggleOff++;
+            } else if(line.equals("Visited Vet Page")){
+                visitedVetToggleOff++;
+            }
+
+            line = logReader.readLine();
+        }
+
+        System.out.println("===== Analyzing Welcome Page to Vet Page Toggle... =====");
+        System.out.println("WITH TOGGLE DISABLED:");
+        System.out.println("users that visited welcome page = " + visitedHomepageToggleOff);
+        System.out.println("users that visited vet page = " + visitedVetToggleOff);
+        System.out.println("calculated conversion ratio = " + (visitedVetToggleOff/visitedHomepageToggleOff));
+        System.out.println("WITH TOGGLE ENABLED:");
+        System.out.println("users that visited welcome page = " + visitedHomepageToggleOn);
+        System.out.println("users that visited vet page = " + visitedVetToggleOn);
+        System.out.println("calculated conversion ratio = " + (visitedVetToggleOff/visitedHomepageToggleOn));
+
         return new RedirectView("/");
     }
 }
